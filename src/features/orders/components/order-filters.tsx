59f6333e90f4fba8ae55b4afac8353f691459751
Diogo Rocha <diogorocha0712@gmail.com>
2026-07-22 15:06:11 -0300

    test: add order filters tests

--- src/features/orders/components/order-filters.tsx
+++ src/features/orders/components/order-filters.tsx
@@ -62,7 +62,7 @@ export function OrderFilters({
               onFilterChange("customerName", event.target.value)
             }
             placeholder='Buscar por nome'
-            className='w-full rounded-lg border border-slate-300 bg-white px-3 py-2.5 text-sm text-slate-900 outline-none transition focus:border-blue-500 focus-rind-2 focus:ring-blue-100'
+            className='w-full rounded-lg border border-slate-300 bg-white px-3 py-2.5 text-sm text-slate-900 outline-none transition focus:border-blue-500 focus:ring-2 focus:ring-blue-100'
           />
         </label>
 
